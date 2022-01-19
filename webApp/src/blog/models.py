6862b46ffebd69601715f460e3bd250ede76318a
Rodrigo Granja <rodrigo.s.granja@hotmail.com>
2022-01-19 15:00:58 +0100

    Refactor and add safety

--- webApp/src/blog/models.py
+++ webApp/src/blog/models.py
@@ -19,7 +19,7 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=120)
     slug = models.SlugField(unique=True)
     #content = models.TextField(null=True, blank=True)
-    content = MarkdownxField(blank=True)
+    content = MarkdownxField(blank=True, null=True)
 
 
     def get_absolute_url(self):
